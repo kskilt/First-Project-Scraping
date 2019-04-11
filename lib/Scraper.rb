a32203c739e332694e49a17a5d3a9a44bdfb4845
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-11 09:15:51 -0400

    adjusted second scraper to have proper variables from scraper one template

--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -29,12 +29,11 @@ class Scraper
       card.css("td").each do |detail|
         stuff << detail.text
       end
-        event = stuff[0]
-        location = stuff[1]
-        date = stuff[4]
-        format = stuff[5]
-        player_count = stuff[7].strip.to_i
-        Tournament.new(event,location,date,format,player_count)
+        rank = stuff[0]
+        player_name = stuff[1]
+        score = stuff[4]
+        squadlist = stuff[7]
+        Squadlist.new(rank, player_name, score, squadlist)
         stuff = []
     end
   end
