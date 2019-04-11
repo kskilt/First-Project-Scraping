500972c74e769af8dff99adc9cdbba5d648790b1
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-04-11 09:10:32 -0400

    changed name of first scraper to tournament scraper and duplicated to start building second scraper

--- lib/CLI.rb
+++ lib/CLI.rb
@@ -1,3 +1,9 @@
 # Handles talking to user, with puts and gets. Does not scrape.
 class CLI
+  attr_accessor :input
+  def call
+    puts "Welcome to Xwing Tournament Results!"
+    puts "Would you like to see the most recent tournaments?"
+  end
+
 end
--- lib/Scraper.rb
+++ lib/Scraper.rb
@@ -1,7 +1,26 @@
 
 # This class handles scraping, and not puts, but it will create other objects
 class Scraper
-  def self.scrape_index_page(url)
+  def self.tournament_scraper(url)
+    index_page = Nokogiri::HTML(open(url))
+    body = index_page.css("tbody")
+    stuff = []
+    e = index_page.css("tbody").css("tr").first.css("td").first.css("a").text
+    details = index_page.css("tbody").css("tr").each do |card|
+      card.css("td").each do |detail|
+        stuff << detail.text
+      end
+        event = stuff[0]
+        location = stuff[1]
+        date = stuff[4]
+        format = stuff[5]
+        player_count = stuff[7].strip.to_i
+        Tournament.new(event,location,date,format,player_count)
+        stuff = []
+    end
+  end
+
+    def self.squad_scraper(url)
     index_page = Nokogiri::HTML(open(url))
     body = index_page.css("tbody")
     stuff = []
